a8c3ceceb01a1b31e4978a2e74cbe83ef125e4f6
Ashley Wolff <jj.wolff496@gmail.com>
2023-04-23 15:04:56 -0700

    aaaaaaaaaaaa
    update files

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,12 @@
 import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
-import Hero from './Hero';
-import Navbar from './Navbar';
-import About from './About';
-import Tech from './Tech';
-import Experience from './Experience';
-import Works from './Works';
-import Feedbacks from './Feedbacks';
-import Contact from './Contact';
+import Hero from './hero';
+import Navbar from './navbar';
+import About from './about';
+import Tech from './tech';
+import Experience from './experience';
+import Works from './works';
+import Feedbacks from './feedbacks';
+import Contact from './contact';
 
 export {
   Hero,
